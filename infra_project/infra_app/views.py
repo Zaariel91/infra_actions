0945cbca91cda67309df4890dee59fd04b14a92c
Nikolay <aleshinkov91@gmail.com>
2023-05-13 22:55:40 +0300

    test

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Exelent!!!')
 
 
 def second_page(request):
